aadc7c231793b9e7d41c14b99a82020372b2e6e7
Sofiane <stamboul.sofiane@gmail.com>
2019-10-15 17:13:18 +0200

    add few tests

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# basicApi
+# Basic API
+
+Basic api with 3 endpoints
+
+## Getting Started
+
+clone the project with the following command
+
+```
+git@github.com:stamtech/basicApi.git
+```
+
+### Prerequisites
+
+Make sure to have nodeJs installed.
+Add an env variable for api url (question 3).
+
+```
+export API_URL= "api url"
+```
+
+### Installing
+
+install all the dependecies to run the api localy
+
+```
+npm ci
+```
+
+## Running the tests
+
+```
+npm test
+```
+
+## Deployment
+
+the deployment is linked to master branch, its use aws code pipeline, code build, and elastic beanstalk
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- src/routes/players.js
+++ src/routes/players.js
@@ -1,39 +1,39 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 
-const data = require('../data.json');
+const data = require("../data.json");
 
 const router = express.Router();
 const getAllPlayers = () => {
-    return data.players;
+  return data.players.sort(sortPlayers);
 };
 const sortPlayers = (a, b) => (a.id > b.id ? 1 : -1);
 const getPlayer = id => data.players.find(player => player.id == id);
 
 const getAllPlayersFromApi = async () => {
-    const response = await axios.get(process.env.API_URL);
-    return response.data.players;
+  const response = await axios.get(process.env.API_URL);
+  return response.data.players;
 };
-router.get('/', (req, res) => {
-    const players = getAllPlayers();
-    const sortedPlayers = players.sort(sortPlayers);
-    res.send(sortedPlayers);
+router.get("/", (req, res) => {
+  const players = getAllPlayers();
+  res.send(players);
 });
 
-router.get('/dynamic/', async (req, res) => {
-    const players = await getAllPlayersFromApi();
-    res.send(players.sort(sortPlayers));
+router.get("/dynamic/", async (req, res) => {
+  const players = await getAllPlayersFromApi();
+  res.send(players.sort(sortPlayers));
 });
 
-router.get('/:id', (req, res) => {
-    const { id } = req.params;
-    const player = getPlayer(id);
-    if (!player) {
-        res.status(404).send(`Player ${id} not found`);
-    }
-    res.send(player);
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const player = getPlayer(id);
+  if (!player) {
+    res.status(404).send(`Player ${id} not found`);
+  }
+  res.send(player);
 });
-
-module.exports.getAllPlayers = getAllPlayers;
 
 module.exports = router;
+module.exports.getAllPlayers = getAllPlayers;
+module.exports.getPlayer = getPlayer;
+module.exports.getAllPlayersFromApi = getAllPlayersFromApi;
